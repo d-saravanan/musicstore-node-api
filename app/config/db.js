4dcd6f9b2466e4b907ce25c8129003b5253ca304
Saravanan Doraiswamy <saravanan@Saravanans-MacBook-Pro-2.local>
2018-12-27 22:24:16 +0530

    updated to read the connection string from the env for heroku deployment

--- app/config/db.js
+++ app/config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    dbUrl: ('mongodb://sa:sa123456@ds233452.mlab.com:33452/music-store')
+    // dbUrl: ('mongodb://sa:sa123456@ds233452.mlab.com:33452/music-store')
+    dbUrl: process.env.DATABASE_URL
 }
