14e6f793ce71ae9d2c6f4c2e776083be362a1b60
Saravanan <s.dorai2009@gmail.com>
2019-01-21 08:09:58 +0530

    testing with a different port number

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const dbConfig = require('./app/config/db');
 
 const app = express();
-const port = process.env.PORT || 80;
+const port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
